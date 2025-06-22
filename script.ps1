4d6e1994e8c91850815cebddec57463d3c588436
#CokeFenya <73313623+CokeFenya@users.noreply.github.com>
2025-06-22 06:16:01 +0500

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,1 +1,10 @@
-Start-Process "C:\Users\danil\AppData\Local\Programs\Microsoft VS Code\Code.exe"
+# Проверка, запущен ли PowerShell с правами администратора
+If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltinRole] "Administrator"))
+{
+    Write-Host "Скрипт запущен НЕ от администратора." -ForegroundColor Red
+}
+else
+{
+    Write-Host "Скрипт запущен ОТ администратора." -ForegroundColor Green
+}
+Pause
