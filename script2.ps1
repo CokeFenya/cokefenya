8bd2dcb8b38123e31a0190006cdf797710f926bf
#CokeFenya <73313623+CokeFenya@users.noreply.github.com>
2025-06-22 10:43:12 +0500

    Update script2.ps1

--- script2.ps1
+++ script2.ps1
@@ -1,5 +1,5 @@
 # === Настройки ===
-$server = "http://5.158.121.185:4200"
+$server = "http://5.158.121.185:4200/api"
 $versionUrl = "$server/version"
 $downloadUrl = "$server/download"
 $regPath = "HKCU:\Software\Zapret"
