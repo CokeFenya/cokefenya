3ddffcf62d3a2f38936c0bb6d0817859282917c1
#CokeFenya <73313623+CokeFenya@users.noreply.github.com>
2025-06-22 06:20:47 +0500

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,11 +1,13 @@
-If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltinRole] "Administrator"))
+[Console]::OutputEncoding = [Text.UTF8Encoding]::new()
+
+if ((New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator))
 {
-    Write-Host "Скрипт запущен НЕ от администратора." -ForegroundColor Red
+    Write-Host "Скрипт запущен от имени администратора"
 }
 else
 {
-    Write-Host "Скрипт запущен ОТ администратора." -ForegroundColor Green
+    Write-Host "Скрипт НЕ запущен от имени администратора"
 }
 
-# Чтобы окно не закрылось, можно просто подождать ввода пользователя:
-Read-Host "Нажмите Enter для выхода..."
+Write-Host "Нажмите Enter для выхода..."
+Read-Host
