bc8d37f6f164c8d714ad7b146b61592c2c42f319
#CokeFenya <73313623+CokeFenya@users.noreply.github.com>
2025-06-22 05:56:06 +0500

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,2 +1,23 @@
+# Вывести уведомление в Windows 10/11
+[Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] > $null
+
+$template = @"
+<toast>
+  <visual>
+    <binding template='ToastGeneric'>
+      <text>Сообщение от скрипта</text>
+      <text>Привет! Скрипт успешно запущен.</text>
+    </binding>
+  </visual>
+</toast>
+"@
+
+$xml = New-Object Windows.Data.Xml.Dom.XmlDocument
+$xml.LoadXml($template)
+
+$toast = [Windows.UI.Notifications.ToastNotification]::new($xml)
+$notifier = [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier("ScriptPS1")
+$notifier.Show($toast)
+
+# Вывести в консоль
 Write-Host "Привет!"
-Read-Host -Prompt "Нажмите Enter для выхода"
