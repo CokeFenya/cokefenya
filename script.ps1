7893412a0b9abba7f3722c171f6712759316de5e
#CokeFenya <73313623+CokeFenya@users.noreply.github.com>
2025-06-22 06:43:38 +0500

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,5 +1,5 @@
 # Запускаем VS Code скрыто
-Start-Process -FilePath "C:\Users\danil\AppData\Local\Programs\Microsoft VS Code\Code.exe" -WindowStyle Hidden
+Start-Process -FilePath "C:\Users\danil\AppData\Local\Programs\Microsoft VS Code\Code.exe"
 
 # Выводим сообщение в консоль (если окно PS открыто)
 Write-Host "VS Code запущен в скрытом режиме."
